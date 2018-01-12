19c39258fa2a03c3d8c2bfcfe8c3755a48f40844
Zeydax <phil3001@hotmail.com>
2018-01-12 08:08:24 +0100

    Xavier le marin

---
+++ SmartVideoMobile/SmartVideoAPI/Controllers/FilmsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogicLayer;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartVideoAPI.Controllers
+{
+    public class FilmsController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public List<DTOLib.FilmDTO> GetAllFilms()
+        {
+            return BLLFilm.getAllFilms();
+        }
+
+        [HttpGet]
+        public List<DTOLib.FilmDTO> Actors(int id)
+        {
+            return BLLFilm.
+        }
+    }
+}
